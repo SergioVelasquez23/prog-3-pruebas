4aa6c22ff2515c509e09829c435c0f0b2478832c
SergioVelasquez23 <sergio.velasquez28396@ucaldas.edu.co>
2025-02-10 11:03:33 -0500

    pipipiip

---
+++ Desktop/git/main.py
@@ -0,0 +1,31 @@
+def Guanajuato(self, x):
+    if x > 10:
+        che = 22
+        result = x+che
+        print("El che: "+ result)
+    else:
+        number = 0
+        result_1 = number + x
+        print("El resultado ingresado por ti, Usuario, es : "+ result_1)
+
+
+def UnKiloLoCompréEnOcho(self, x, y):
+    result = x + y
+    print(result)
+    
+
+def VendimosMedioEnTrece(self, a, b):
+    result = a * b
+    print(result)
+    
+    
+def RecibimosLukGringa(self, c, d):
+    result = c / d
+    print(result)
+    
+    
+UnKiloLoCompréEnOcho(4, 3, 55)
+
+VendimosMedioEnTrece(4, 99, 989)
+
+RecibimosLukGringa(5, 29, 11)
